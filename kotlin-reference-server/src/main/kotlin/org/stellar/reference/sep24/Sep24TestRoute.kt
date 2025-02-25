a3e2ed7a08c545c932b8c461c1d5a4162f80b783
jiahuihu <jiahuihu0425@gmail.com>
2025-02-25 13:11:19 -0500

    use call.request.queryParameters

--- kotlin-reference-server/src/main/kotlin/org/stellar/reference/sep24/Sep24TestRoute.kt
+++ kotlin-reference-server/src/main/kotlin/org/stellar/reference/sep24/Sep24TestRoute.kt
@@ -24,11 +24,13 @@ fun Route.testSep24(
 ) {
   route("/sep24/interactive") {
     get {
-      log.info { "Called /sep24/interactive with parameters ${call.parameters}" }
+      log.info {
+        "Called /sep24/interactive with parameters ${call.request.queryParameters.entries()}"
+      }
 
       val token =
         JwtDecoder.decode(
-          call.parameters["token"]
+          call.request.queryParameters["token"]
             ?: throw ClientException("Missing token parameter in the request"),
           jwtKey,
         )
