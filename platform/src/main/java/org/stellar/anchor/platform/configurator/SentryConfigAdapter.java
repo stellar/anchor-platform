1051b3929f36f3cf3f1938a84e5a78f8b50dd2ab
Philip Liu <12836897+philipliu@users.noreply.github.com>
2025-03-12 13:32:21 -0400

    Remove message on Sentry initialization (#1677)
    ### Description
    
    This removes the message sent to Sentry on initialization.
    
    ### Context
    
    This is creating a lot of noise. Now that we have Prometheus alerts on
    crashing pods, it is unnecessary.
    
    ### Testing
    
    - `./gradlew test`
    
    ### Documentation
    
    N/A
    
    ### Known limitations
    
    N/A

--- platform/src/main/java/org/stellar/anchor/platform/configurator/SentryConfigAdapter.java
+++ platform/src/main/java/org/stellar/anchor/platform/configurator/SentryConfigAdapter.java
@@ -1,6 +1,5 @@
 package org.stellar.anchor.platform.configurator;
 
-import static io.sentry.Sentry.captureMessage;
 import static org.stellar.anchor.util.Log.*;
 import static org.stellar.anchor.util.StringHelper.*;
 
@@ -31,11 +30,6 @@ public class SentryConfigAdapter extends SpringConfigAdapter {
           options.setTracesSampleRate(1.0);
           options.setEnableUncaughtExceptionHandler(true);
         });
-
-    captureMessage(
-        String.format(
-            "Sentry agent initialized. release:%s, environment: %s, debug: %s",
-            config.getString("sentry.release"), sentryEnv, config.getBoolean("sentry.debug")));
   }
 
   String getAuthToken() {
