13891504618d198018656beae46265aa27b16a60
Jamie Li <lijamie@gmail.com>
2025-04-17 10:31:00 -0700

    revert test profile

--- essential-tests/src/testFixtures/kotlin/org/stellar/anchor/platform/AbstractIntegrationTests.kt
+++ essential-tests/src/testFixtures/kotlin/org/stellar/anchor/platform/AbstractIntegrationTests.kt
@@ -2,6 +2,7 @@ package org.stellar.anchor.platform
 
 import io.ktor.client.plugins.*
 import io.ktor.http.*
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.retryWhen
@@ -20,7 +21,6 @@ import org.stellar.walletsdk.Wallet
 import org.stellar.walletsdk.anchor.auth
 import org.stellar.walletsdk.auth.AuthToken
 import org.stellar.walletsdk.horizon.SigningKeyPair
-import kotlin.time.Duration.Companion.seconds
 
 abstract class AbstractIntegrationTests(val config: TestConfig) {
   companion object {
@@ -34,13 +34,13 @@ abstract class AbstractIntegrationTests(val config: TestConfig) {
     // the test payment values
     private const val TEST_PAYMENT_MEMO = "22bf7341574e4b1082516a2e84a8"
     private const val TEST_PAYMENT_DEST_ACCOUNT =
-        "GBDYDBJKQBJK4GY4V7FAONSFF2IBJSKNTBYJ65F5KCGBY2BIGPGGLJOH"
+      "GBDYDBJKQBJK4GY4V7FAONSFF2IBJSKNTBYJ65F5KCGBY2BIGPGGLJOH"
     private const val TEST_PAYMENT_ASSET_CIRCLE_USDC =
-        "USDC:GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5"
+      "USDC:GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5"
 
     // custody deposit address
     private const val CUSTODY_DEST_ACCOUNT =
-        "GC6X2ANA2OS3O2ESHUV6X44NH6J46EP2EO2JB7563Y7DYOIXFKHMHJ5O"
+      "GC6X2ANA2OS3O2ESHUV6X44NH6J46EP2EO2JB7563Y7DYOIXFKHMHJ5O"
 
     private lateinit var testPaymentValues: List<Pair<String, String>>
 
@@ -62,28 +62,29 @@ abstract class AbstractIntegrationTests(val config: TestConfig) {
       val destAccount = TEST_PAYMENT_DEST_ACCOUNT
       val horizonServer = Server("https://horizon-testnet.stellar.org")
       val payments =
-          horizonServer
-              .payments()
-              .forAccount(destAccount)
-              .order(RequestBuilder.Order.DESC)
-              .includeTransactions(true)
-              .limit(100)
-              .execute()
-              .records
+        horizonServer
+          .payments()
+          .forAccount(destAccount)
+          .order(RequestBuilder.Order.DESC)
+          .includeTransactions(true)
+          .limit(100)
+          .execute()
+          .records
       for (payment in payments) {
         if (payment is PaymentOperationResponse) {
           if (payment.transaction.memo.toString() == TEST_PAYMENT_MEMO) {
             println("Found test payment")
             // initialize the test payment value pairs for injection
             testPaymentValues =
-                listOf(
-                    Pair("%TESTPAYMENT_ID%", payment.id.toString()),
-                    Pair("%TESTPAYMENT_AMOUNT%", payment.amount),
-                    Pair("%TESTPAYMENT_TXN_HASH%", payment.transactionHash),
-                    Pair("%TESTPAYMENT_SRC_ACCOUNT%", payment.from),
-                    Pair("%TESTPAYMENT_DEST_ACCOUNT%", payment.to),
-                    Pair("%TESTPAYMENT_ASSET_CIRCLE_USDC%", TEST_PAYMENT_ASSET_CIRCLE_USDC),
-                    Pair("%CUSTODY_DEST_ACCOUNT%", CUSTODY_DEST_ACCOUNT))
+              listOf(
+                Pair("%TESTPAYMENT_ID%", payment.id.toString()),
+                Pair("%TESTPAYMENT_AMOUNT%", payment.amount),
+                Pair("%TESTPAYMENT_TXN_HASH%", payment.transactionHash),
+                Pair("%TESTPAYMENT_SRC_ACCOUNT%", payment.from),
+                Pair("%TESTPAYMENT_DEST_ACCOUNT%", payment.to),
+                Pair("%TESTPAYMENT_ASSET_CIRCLE_USDC%", TEST_PAYMENT_ASSET_CIRCLE_USDC),
+                Pair("%CUSTODY_DEST_ACCOUNT%", CUSTODY_DEST_ACCOUNT)
+              )
 
             return
           }
@@ -93,7 +94,8 @@ abstract class AbstractIntegrationTests(val config: TestConfig) {
       println("\n*** STOP ***")
       println("Cannot find test payment")
       println(
-          "Please visit the testnet reset script: https://github.com/stellar/useful-scripts to create the test payment")
+        "Please visit the testnet reset script: https://github.com/stellar/useful-scripts to create the test payment"
+      )
       throw Exception("Cannot find test payment")
     }
   }
@@ -104,11 +106,12 @@ abstract class AbstractIntegrationTests(val config: TestConfig) {
   }
 
   var toml: TomlContent =
-      parse(resourceAsString("${config.env["anchor.domain"]}/.well-known/stellar.toml"))
+    parse(resourceAsString("${config.env["anchor.domain"]}/.well-known/stellar.toml"))
   var wallet =
-      Wallet(
-          StellarConfiguration.Testnet,
-          ApplicationConfiguration { defaultRequest { url { protocol = URLProtocol.HTTP } } })
+    Wallet(
+      StellarConfiguration.Testnet,
+      ApplicationConfiguration { defaultRequest { url { protocol = URLProtocol.HTTP } } }
+    )
   var walletKeyPair = SigningKeyPair.fromSecret(CLIENT_WALLET_SECRET)
   var anchor = wallet.anchor(config.env["anchor.domain"]!!)
   var token: AuthToken
@@ -116,20 +119,20 @@ abstract class AbstractIntegrationTests(val config: TestConfig) {
   private val submissionLock = Mutex()
 
   suspend fun transactionWithRetry(
-      maxAttempts: Int = 5,
-      delay: Int = 5,
-      transactionLogic: suspend () -> Unit
+    maxAttempts: Int = 5,
+    delay: Int = 5,
+    transactionLogic: suspend () -> Unit
   ) =
-      flow<Unit> { submissionLock.withLock { transactionLogic() } }
-          .retryWhen { _, attempt ->
-            if (attempt < maxAttempts) {
-              delay((delay + (1..5).random()).seconds)
-              return@retryWhen true
-            } else {
-              return@retryWhen false
-            }
-          }
-          .collect {}
+    flow<Unit> { submissionLock.withLock { transactionLogic() } }
+      .retryWhen { _, attempt ->
+        if (attempt < maxAttempts) {
+          delay((delay + (1..5).random()).seconds)
+          return@retryWhen true
+        } else {
+          return@retryWhen false
+        }
+      }
+      .collect {}
 
   init {
     runBlocking { token = anchor.auth().authenticate(walletKeyPair) }
