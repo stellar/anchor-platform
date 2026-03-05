bbd28b3e31d27c09e80e16a38a7fac4b7713325e
Philip Liu <12836897+philipliu@users.noreply.github.com>
2026-03-05 10:39:08 -0500

    [ANCHOR-1153] Limit SEP-1 response body size and redirects (#1894)

--- lib-util/src/main/java/org/stellar/anchor/util/FileUtil.java
+++ lib-util/src/main/java/org/stellar/anchor/util/FileUtil.java
@@ -1,9 +1,11 @@
 package org.stellar.anchor.util;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Collectors;
@@ -26,5 +28,24 @@ public class FileUtil {
 
   public static String read(Path path) throws IOException {
     return Files.readString(path);
+  }
+
+  public static String read(Path path, long maxSize) throws IOException {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    byte[] buffer = new byte[8192];
+    long totalRead = 0;
+
+    try (InputStream inputStream = Files.newInputStream(path)) {
+      int read;
+      while ((read = inputStream.read(buffer)) != -1) {
+        totalRead += read;
+        if (totalRead > maxSize) {
+          throw new IOException("File exceeds max size of " + maxSize + " bytes");
+        }
+        outputStream.write(buffer, 0, read);
+      }
+    }
+
+    return new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
   }
 }
