428a0d4ac53afdc304357219db5594500d29362d
jiahuihu <jiahuihu0425@gmail.com>
2026-03-23 13:36:05 -0400

    fix merging overwrites

--- platform/src/main/java/org/stellar/anchor/platform/service/TransactionService.java
+++ platform/src/main/java/org/stellar/anchor/platform/service/TransactionService.java
@@ -348,7 +348,8 @@ public class TransactionService {
           break;
       }
     } catch (OptimisticLockingFailureException ex) {
-      throw new BadRequestException("Transaction was modified by another request. Please re-read the transaction state and retry if appropriate.");
+      throw new BadRequestException(
+          "Transaction was modified by another request. Please re-read the transaction state and retry if appropriate.");
     }
 
     return PlatformTransactionHelper.toGetTransactionResponse(txn, assetService);
