c7a54218fd5572efd304ce0d9937e2f703335a3a
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2026-05-14 15:00:15 -0300

    refactor(platform): reorder imports
    * reorder `jakarta.transaction.transactional` import

--- platform/src/main/java/org/stellar/anchor/platform/data/JdbcSep38QuoteRepo.java
+++ platform/src/main/java/org/stellar/anchor/platform/data/JdbcSep38QuoteRepo.java
@@ -1,14 +1,12 @@
 package org.stellar.anchor.platform.data;
 
+import jakarta.transaction.Transactional;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
-
-import jakarta.transaction.Transactional;
 
 public interface JdbcSep38QuoteRepo extends CrudRepository<JdbcSep38Quote, String> {
   Optional<JdbcSep38Quote> findById(@NonNull String id);
