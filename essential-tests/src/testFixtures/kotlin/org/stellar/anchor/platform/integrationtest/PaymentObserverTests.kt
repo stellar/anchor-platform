37c5447d4838e1c1d8fae463cf3e28609bcef289
Amanda Gonsalves <64379712+amandagonsalves@users.noreply.github.com>
2026-05-21 15:23:02 -0300

    refactor(test): update test failure assertion
    * update test assertion from `assertNotNull(null)` to `fail()`
    
    * refactor test failure logic for clarity and explicitness

--- essential-tests/src/testFixtures/kotlin/org/stellar/anchor/platform/integrationtest/PaymentObserverTests.kt
+++ essential-tests/src/testFixtures/kotlin/org/stellar/anchor/platform/integrationtest/PaymentObserverTests.kt
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.stellar.anchor.asset.AssetService
@@ -358,9 +359,7 @@ class PaymentObserverTests {
       }
       delay(1000)
     }
-    assertNotNull(null) {
-      "Timed out after ${timeout}ms waiting for event from account $fromAccountId"
-    }
+    fail("Timed out after ${timeout}ms waiting for event from account $fromAccountId")
   }
 
   private fun sendTestPayment(fromKeyPair: KeyPair, toKeyPair: KeyPair): Transaction {
